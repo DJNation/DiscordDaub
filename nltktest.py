b0feb4d02ae9906554a3b4047c970a49a3fbe307
Dakota Nation <djnati6387@ung.edu>
2018-04-08 17:57:10 -0400

    Updated

--- nltktest.py
+++ nltktest.py
@@ -10,10 +10,6 @@ prepArr = [     "Forsooth, ",
 		"Forsooth, sayeth I, ",
 		"Hark, ",
 		"Harketh, ",
-		"By &god, ",
-		"By the Will of &godadj &god, ",
-		"By the &bodyadj &bodypart of the &godadj &god, ",
-		"By &godadj &god's &bodyadj &bodypart, ",
 		"Avast, ",
 		"Zounds, ",
 		"Perchance, ",
@@ -37,8 +33,6 @@ prepArr = [     "Forsooth, ",
 		"By my word, ",
 		"S'wounds, ",
 		"Z'wounds, ",
-		"&god's wounds, ",
-		"&god's &bodypart, ",
 		"Heigh-ho, ",
 		"Ah, ",
 		"Quoth I, ",
@@ -50,6 +44,13 @@ prepArr = [     "Forsooth, ",
 		"Come, ",
 		"Kind sire, ",
 		"Sire, ",]
+
+prepGod = [     "&god's wounds, ",
+		"&god's &bodypart, ",
+                "By &god, ",
+		"By the Will of &godadj &god, ",
+		"By the &bodyadj &bodypart of the &godadj &god, ",
+		"By &godadj &god's &bodyadj &bodypart, ",]
 
 postArr = [     " Anon!",
 		" Hum.",
@@ -97,7 +98,7 @@ youArr = [      "thou",
 lolArr = [      "lolleth",
                 "lollery"]
 
-killPastArr = [     "slain",
+killPastArr = [ "slain",
 		"vanquished",
 		"brung low",
 		"conquered",
@@ -117,6 +118,99 @@ killArr = [     "slay",
 		"best",
 		"foil"]
 
+personal = [    'town',
+                'village',
+                'home',
+                
+                'lol',
+                
+                'afk',
+                
+                'rofl'
+                
+                'water',
+                
+                'balls',
+                'groin',
+                
+                'gold',
+                'money',
+                
+                'joke',
+                
+                'idiot',
+                'fool',
+                'bastard',
+                'moron',
+                'idiots',
+                'fools',
+                'bastards',
+                'morons',
+                'fucker',
+                'fuckers',
+                'shit',
+                
+                'map',
+                'maps',
+                
+                'debuff',
+                'debuffed',
+                
+                'food',
+                
+                'man',
+                'guy',
+                'guys',
+                'men',
+                'boys',
+                
+                'party',
+                'group',
+                
+                'noob',
+                'newb',
+                'newbie',
+                'nub',
+                'lowbie',
+                'beginner',
+                'noobs',
+                'newbs',
+                'newbies',
+                'nubs',
+                'lowbies',
+                'beginners',
+                
+                'level',
+                
+                'hehe',
+                'haha',
+                'heh',
+                'hah',
+                
+                'shop',
+                'store',
+                'vendor',
+                'seller',
+                
+                'friend',
+                'buddy',
+                'pal',
+                'mate',
+                'friends',
+                'buddies',
+                'pals',
+                'mates',
+                
+                'girl',
+                'woman',
+                'girls',
+                'women',
+                
+                'child',
+                'flag',
+                
+                'sick']
+
 def medieval(s): #main function
     s = checkTags(s)
     s = nounReplacements(s)
@@ -130,8 +224,8 @@ def prepend(): #prepends the message with a phrase
 def nounReplacements(s):    #replaces improper nouns (school, tree, plant) with a rate of .33
     pos = nltk.pos_tag(s)
     for i in range(len(s)):
-        rand = random.randint(1,101)
-        if pos[i][1] == 'NN':
+        rand = rando()
+        if pos[i][1] == 'NN' and personalized(s[i]) != True: #Checks if word is a noun and is not changed by particular replacement
             x = s[i]
             if(x[0].isupper()): continue #will ignore intentional capitalizations of nouns (for usernames, etc)
             if rand > 34: continue #randomizer
@@ -140,7 +234,12 @@ def nounReplacements(s):    #replaces improper nouns (school, tree, plant) with 
             else:
                 s[i] = x+"eth"
     return s
-    
+
+def personalized(s): #Checks if noun in question is in the personal list
+        if s in personal:
+            return True
+        else:
+            return False
 
 def wordReplacements(tag): #Replaces phrases and single words
     #for preceeding "it"
@@ -161,11 +260,184 @@ def wordReplacements(tag): #Replaces phrases and single words
             if(tag[word+1] == 'were'):
                 tag[word] = "'twere"
                 tag[word+1] = ''
-    #for succeeding "not"
+
+            if(tag[word] == 'teh'):
+                tag[word] == 'the'
     for word in range(len(tag)):
-        if((tag[word] == 'shall' or tag[word] == 'will')  and tag[word+1] == 'not'):
+        if((tag[word] == 'shall' or tag[word] == 'will')  and tag[word+1] == 'not'): #shant
             tag[word] = "shan't"
             tag[word+1] = ''
+        if(tag[word] == 'is'): #is
+            rand = rando()
+            if(rand > 50):
+                tag[word] = 'be'
+        if(tag[word] == 'map'): #map
+            tag[word] = 'chart'
+        if(tag[word] == 'between'): #between
+            tag[word] = 'betwixt'
+        if(tag[word] == 'aggro'): #aggro
+            tag[word] = 'wrath'
+        if(tag[word] == 'buy'): #buy
+            rand = rando()
+            if(rand > 50):
+                tag[word] = 'purchase'
+            else: tag[word] = 'obtain'
+        if(tag[word] == 'bought'): #bought
+            rand = rando()
+            if(rand > 50):
+                tag[word] = 'purchased'
+            else: tag[word] = 'obtained'
+        if(tag[word] == 'debuff'): #debuff
+            rand = rando()
+            if(rand > 1 and rand <= 33):
+                tag[word] = 'ailment' 
+            elif(rand > 34 and rand <= 66):
+                tag[word] = 'sickness'
+            else: tag[word] = 'pox'
+        if(tag[word] == 'debuffed' or tag[word] == 'sick'): #debuffed / sick
+            rand = rando()
+            if(rand > 1 and rand <= 33):
+                tag[word] = 'ailed'
+            elif(rand > 34 and rand <= 66):
+                tag[word] = 'sicknened'
+            else: tag[word] = "pox't"
+        if(tag[word] == 'sell'): #sell
+            rand = rando()
+            if(rand > 1 and rand <= 25):
+                tag[word] = 'hawk'
+            elif(rand > 25 and rand <= 50):
+                tag[word] = 'pawn'
+            elif(rand > 50 and rand <= 75):
+                tag[word] = "tender"
+            else: tag[word] ='purvey'
+        if(tag[word] == 'sold'): #sold
+            rand = rando()
+            if(rand > 1 and rand <= 25):
+                tag[word] = "hawk'd"
+            elif(rand > 25 and rand <= 50):
+                tag[word] = 'pawned'
+            elif(rand > 50 and rand <= 75):
+                tag[word] = "tendered"
+            else: tag[word] = 'purveyed'
+        if(tag[word] == 'food'): #food
+            rand = rando()
+            print(rand)
+            if(rand > 1 and rand < 20):
+                print(True)
+                tag[word] = "vittles"
+            elif(rand > 20 and rand < 40):
+                tag[word] = 'rations'
+            elif(rand > 40 and rand < 60):
+                tag[word] = "sustenance"
+            elif(rand > 60 and rand < 80):
+                tag[word] = "sustenance"
+            else: tag[word] = 'viands'
+        if(tag[word]  == 'bet'):
+           tag[word] = 'warrant'
+        if(tag[word] == 'hunting' or tag[word] == 'huntin'):
+            tag[word] = "a-huntin'"
+        if(tag[word] == 'coming' or tag[word] == 'comin'):
+            tag[word] = "a-comin'"
+        if(tag[word] == 'walking' or tag[word] == 'walkin'):
+            tag[word] = "a-walkin'"
+        if(tag[word] == 'making' or tag[word] == 'makin'):
+            tag[word] = "a-makin'"
+        if(tag[word] == 'of'):
+            rand = rando()
+            if rand >= 50:
+                tag[word] = "o'"
+        if(tag[word] == 'away'):
+            rand = rando()
+            if rand > 74:
+                tag[word] = 'aroint'
+        if(tag[word] == 'being'):
+            rand = rando()
+            if rand > 25:
+                tag[word] = "bein'"
+        if(tag[word] == 'why'):
+            rand = rando()
+            if rand > 50:
+                tag[word] = 'wherefore'
+        if(tag[word] == 'fucker'):
+            tag[word] = 'swiver'
+        if(tag[word] == 'fuckers'):
+            tag[word] = 'swivers'
+        if(tag[word] == 'shit'):
+            tag[word] = 'nightsoil'
+        if(tag[word] == 'child'):
+            rand = rando()
+            if rand > 50:
+                tag[word] = 'poppet'
+        if(tag[word] == 'those'):
+            rand = rando()
+            if rand > 50:
+                tag[word] = 'yon'
+        if(tag[word] == 'really'):
+            rand = rando()
+            if rand > 50:
+                tag[word] = 'indeed'
+            else: tag[word] = 'in truth'
+        if(tag[word] == 'often'):
+            rand = rando()
+            if rand > 33:
+                tag[word] = 'oft'
+        if(tag[word] == 'maybe'):
+            rand = rando()
+            if rand > 33:
+                tag[word] = 'mayhaps'
+            if rand > 66:
+                tag[word] = 'perchance'
+        if(tag[word] == 'sure'):
+            rand = rando()
+            if rand > 66:
+                tag[word] = 'shore'
+        if(tag[word] == 'assist'): #food
+            rand = rando()
+            if(rand > 1 and rand <= 20):
+                tag[word] = "aid"
+            elif(rand > 20 and rand <= 40):
+                tag[word] = 'aideth'
+            elif(rand > 40 and rand <= 60):
+                tag[word] = "saveth"
+            elif(rand > 60 and rand <= 80):
+                tag[word] = "assistance"
+            else: tag[word] = 'succor'
+        if(tag[word] == 'could'):
+            rand = rando()
+            if rand > 50:
+                tag[word] = 'couldst'
+        if(tag[word] == 'would'):
+            rand = rando()
+            if rand > 50:
+                tag[word] = 'wouldst'
+        if(tag[word] == 'later'):
+            rand = rando()
+            if rand > 50:
+                tag[word] = 'anon'
+        if(tag[word] == 'here'):
+            rand = rando()
+            if rand > 25:
+                tag[word] = 'hither'
+        if(tag[word] == 'enough'):
+            rand = rando()
+            if rand > 66:
+                tag[word] = 'enow'
+        if(tag[word] == 'though'):
+            rand = rando()
+            if rand > 66:
+                tag[word] = "tho'"
+        if(tag[word] == 'underneath' or tag[word] == 'beneath' or tag[word] == 'under'):
+            rand = rando()
+            if rand > 50:
+                tag[word] = " 'neath"
+        if(tag[word] == 'until'):
+            rand = rando()
+            if rand > 33:
+                tag[word] = "' till"
+
+
+        
+        
     #contractions
     #for word in range(len(tag)):
     #misc
@@ -270,13 +542,17 @@ def postpend(s): # for '.' end punct
     x = random.randrange(len(postArr)-1)
     return (s + postArr[x])
 
+def rando():
+    rand = random.randint(1,101)
+    return rand
+
 
 #TESTING--------------------------------TESTING
-#while True:
-#    s = input("Enter a string ")
-#    print(s)
-#    print(medieval(s))
-#    s = input("Again?")
-#    if(s == 'n'):
-#        break
+while True:
+    s = input("Enter a string ")
+    print(s)
+    print(medieval(s))
+    s = input("Again?")
+    if(s == 'n'):
+        break
 #TESTING--------------------------------TESTING
